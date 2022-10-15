4251c35d1a511742bb580300f962b532c72dfb5e
Aselemi Divine <aselemidivine@gmail.com>
2022-10-15 16:31:41 -0700

    Made it responsive

---
+++ script.js
@@ -0,0 +1,29 @@
+
+// const productContainers = [...document.querySelectorAll('.product-container')];
+// const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+// const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+// productContainers.forEach((item, i) => {
+//     let containerDimentions = item.getBoundingClientRect();
+//     let containerWidth = containerDimentions.width;
+
+//     nxtBtn[i].addEventListener('click', () => {
+//         item.scrollLeft += containerWidth;
+//     })
+    
+//     preBtn[i].addEventListener('click', () => {
+//         item.scrollLeft -= containerWidth;
+//     })
+// })
+
+// scroll to top 
+
+const toTop = document.querySelector (".to-top");
+window.addEventListener("scroll", () => {
+    if  (window.pageYOffset > 100)  {
+        toTop.classList.add("active");
+    }
+    else {
+        toTop.classList.remove ("active");
+    }
+})
